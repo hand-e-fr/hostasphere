ad0413098bc03f9e24c20ca8525bf3daf2e59d72
William Jolivet <william.jolivet@epitech.eu>
2024-09-12 17:00:14 +0200

    add calls history

--- app/front/src/components/dashboard/step/FuncCalls.tsx
+++ app/front/src/components/dashboard/step/FuncCalls.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { ProfilerData } from "@/types/ProfilerData";
 
 interface ExecutionDiagramProps {
@@ -6,33 +6,77 @@ interface ExecutionDiagramProps {
 }
 
 const colors: string[] = [
-    'step-accent',
-    'step-info',
-    'step-warning',
-    'step-error'
+    'step-primary',
+    'step-secondary',
 ]
 
 const FuncCalls: React.FC<ExecutionDiagramProps> = ({ profilerData }) => {
+    const [targetFunction, setTargetFunction] = useState<ProfilerData | null>(null);
+
+    if (profilerData.length === 0) {
+        return <div>No data</div>;
+    }
+
+    profilerData.sort((a, b) => a.starttime - b.starttime);
+    const start = profilerData[0].starttime;
+
     return (
         <>
-            <div className="flex justify-center">
-                <ul className="steps steps-vertical">
-                    {profilerData.map((data, index) => (
-                        <li key={index} className={`step ${colors[index % 4]}`}>
-                            {data.functionname}
-                        </li>
-                    ))}
-                </ul>
+            <div className="flex justify-center mt-10">
                 <div>
-                    <ul className="">
+                    <ul className="steps steps-vertical">
                         {profilerData.map((data, index) => (
-                            <li key={index} className={`mt-8`}>
-                                <button className="btn btn-sm btn-primary">
-                                    {data.functionname}
-                                </button>
+                            <li key={index} className={`step step-accent`} data-content={``}
+                                onClick={() => setTargetFunction(data)}>
+                                <div className="flex items-center">
+                                    <div className="badge">{data.starttime - start}ms</div>
+                                    <p className="m-2">-</p>
+                                    <div className="badge">{data.functionname}</div>
+                                </div>
                             </li>
                         ))}
                     </ul>
+                </div>
+                <div className="divider lg:divider-horizontal"></div>
+                <div id="func-calls" className="sticky top-0">
+                    {
+                        targetFunction ? (
+                            <div>
+                                <h1 className="text-center text-l underline bold">{targetFunction.functionname}:</h1>
+                                <ul className="steps steps-vertical">
+                                    {targetFunction.functioncallers && targetFunction.functioncallers.map((data, index) => (
+                                        <li key={index} className={`step step-warning`} data-content={``}>
+                                            <div className="flex items-center">
+                                                <div className="badge">{data.caller}</div>
+                                                <p className="m-2">-</p>
+                                                <div className="badge">{data.callerfile}:{data.callerline}</div>
+                                            </div>
+                                        </li>
+                                    ))}
+                                    <li className={`step step-accent`} data-content={``}>
+                                        <div className="flex items-center">
+                                            <div className="badge">{targetFunction.starttime - start}ms</div>
+                                            <p className="m-2">-</p>
+                                            <div className="badge">{targetFunction.functionname}</div>
+                                        </div>
+                                    </li>
+                                    <li className={`step step-info`} data-content={``}>
+                                        <div className="flex items-center">
+                                            <div className="badge">{targetFunction.returnedvalue.type}</div>
+                                            <p className="m-2">-</p>
+                                            <div className="badge">{targetFunction.returnedvalue.value}</div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        ) : (
+                            <div className="bg-gray-100 border rounded-lg m-4 h-full">
+                                <div className="flex justify-center mt-72">
+                                    <h1>Click on a function to see its calls</h1>
+                                </div>
+                            </div>
+                        )
+                    }
                 </div>
             </div>
         </>
