d85b7e8252ef3202c3126f8f23e7ea4ad09ee7cb
William Jolivet <william.jolivet@epitech.eu>
2024-09-12 08:49:01 +0200

    rename containers on shell script on front

--- app/front/restart.sh
+++ app/front/restart.sh
@@ -1,3 +1,3 @@
-docker rm -f front
+docker rm -f hostasphere-front
 docker build -t handoe/hostasphere-front:latest .
-docker run -d --name front -p 3000:3000 --network my_network --restart unless-stopped handoe/hostasphere-front:latest
+docker run -d --name hostasphere-front -p 3000:3000 --network my_network --restart unless-stopped handoe/hostasphere-front:latest
