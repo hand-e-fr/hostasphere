93558256fd82e06e08ad322b5092e33dbce0a3cd
William Jolivet <william.jolivet@epitech.eu>
2024-10-04 14:57:04 +0200

    add: filter and sort options

--- server/pricing/app.py
+++ server/pricing/app.py
@@ -2,27 +2,52 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 from llm_price_scraper.enums import DataSources
 from llm_price_scraper.scrapers import LlmPricingScraper
+from functools import cmp_to_key
+import time
 
 app = Flask(__name__)
-
 CORS(app)
+
+cache = {}
+
+CACHE_TIMEOUT = 3600
+
+def is_cache_valid(cached_time):
+    """Check if cache is still valid."""
+    current_time = time.time()
+    return (current_time - cached_time) < CACHE_TIMEOUT
 
 @app.route('/api/pricing/', methods=['GET'])
 def get_pricing():
     source = request.args.get('source', default='docsbot', type=str)
 
+    model_name_filter = request.args.get('model_name', default=None, type=str)
+    sort_by = request.args.get('sort_by', default=None, type=str)
+    order = request.args.get('order', default='asc', type=str)
+
     try:
+        source_enum_value = None
         if source.lower() == DataSources.DOCSBOT.value:
-            results = LlmPricingScraper.scrape(DataSources.DOCSBOT)
+            source_enum_value = DataSources.DOCSBOT
         elif source.lower() == DataSources.BOTGENUITY.value:
-            results = LlmPricingScraper.scrape(DataSources.BOTGENUITY)
+            source_enum_value = DataSources.BOTGENUITY
         elif source.lower() == DataSources.HUGGINGFACE.value:
-            results = LlmPricingScraper.scrape(DataSources.HUGGINGFACE)
+            source_enum_value = DataSources.HUGGINGFACE
         elif source.lower() == DataSources.HUHUHANG.value:
-            results = LlmPricingScraper.scrape(DataSources.HUHUHANG)
+            source_enum_value = DataSources.HUHUHANG
         else:
             return jsonify(
                 {"error": f"Source '{source}' is not supported."}), 400
+
+        cache_entry = cache.get(source_enum_value)
+        if cache_entry and is_cache_valid(cache_entry['time']):
+            results = cache_entry['data']
+        else:
+            results = LlmPricingScraper.scrape(source_enum_value)
+            cache[source_enum_value] = {
+                'data': results,
+                'time': time.time()
+            }
 
         pricing_data = [
             {
@@ -36,6 +61,18 @@ def get_pricing():
             }
             for result in results
         ]
+
+        if model_name_filter:
+            pricing_data = [
+                item for item in pricing_data if model_name_filter.lower() in item['model'].lower()
+            ]
+
+        if sort_by:
+            if sort_by not in pricing_data[0]:
+                return jsonify({"error": f"Cannot sort by '{sort_by}'."}), 400
+
+            reverse = True if order.lower() == 'desc' else False
+            pricing_data = sorted(pricing_data, key=lambda x: x[sort_by], reverse=reverse)
 
         return jsonify({"pricing_data": pricing_data}), 200
 
